0923ad15e487312d56b4f98d98c5abb18ed71e65
Riddick <roderickjgonzalez@outlook.com>
2019-07-05 15:43:13 -0500

    Creacion de conexiones Login Validacion Usuarios Cerrar Sesion

--- app/src/main/java/com/example/reacreditacion/MainActivity.java
+++ app/src/main/java/com/example/reacreditacion/MainActivity.java
@@ -1,15 +1,62 @@
 package com.example.reacreditacion;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+    //Variable para inicio de sesion Firebase
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        // Inicializamos el inicio de sesion de Firebase
+        mAuth = FirebaseAuth.getInstance();
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+    //Al seleccionar una opcion esta toma su ID para hacer lo debido
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.salir:
+                cerrarSesion();
+                break;
+            case R.id.acercade:
+                //irAcercaDe();
+                break;
+        }
+        return true;
+    }
+
+    public void cerrarSesion(){
+        //Cierra la sesion
+        FirebaseAuth.getInstance().signOut();
+        finish();
+        //Lo envia al login
+        irLogin();
+
+    }
+
+    public void irLogin(){
+        Intent intent = new Intent(this, Login.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
     }
 
 }
